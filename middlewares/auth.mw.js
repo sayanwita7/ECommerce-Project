8155b67e053ec4734f4eb1652f04ab321fc159be
sayanwita7 <dey.sayanwita7@gmail.com>
2024-10-23 02:53:58 +0530

    Login with M/W

--- middlewares/auth.mw.js
+++ middlewares/auth.mw.js
@@ -39,6 +39,21 @@ const user_model = require("../models/user.models")
     }
  }
 
+const verifySignInBody = async (req, res, next)=>{
+    if (!req.body.userId){
+        return res.status(400).send({
+            message: "userId is not provided"
+        })
+    }
+    if (!req.body.password){
+        return res.status(400).send({
+            message: "password is not provided"
+        })
+    }
+    next()
+}
+
  module.exports = {
-    verifySignUpBody: verifySignUpBody
+    verifySignUpBody: verifySignUpBody,
+    verifySignInBody: verifySignInBody
  }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
 // Need to intercept : POST localhost:8888/ecomm/api/v1/auth/signup
+//Middleware is always in the form of an array
 
 const authController = require ("../controllers/auth.controller")
 const authMW = require ("../middlewares/auth.mw")
@@ -6,5 +7,5 @@ module.exports = (app) =>{
     app.post ("/ecomm/api/v1/auth/signup", [authMW.verifySignUpBody], authController.signup)
 
     //Route for POST localhost:8888/ecomm/api/v1/auth/signin
-    app.post("/ecomm/api/v1/auth/signin", authController.signin)
+    app.post("/ecomm/api/v1/auth/signin", [authMW.verifySignInBody],authController.signin)
 }
