e3dbdadca720fb68375b16fbe6f3895f54e8893f
sayanwita7 <dey.sayanwita7@gmail.com>
2024-10-23 02:38:20 +0530

    Login API

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,9 @@
 //Logic to register a user
 const bcrypt = require ("bcryptjs")
 const user_model = require("../models/user.models")
+const jwt = require ("jsonwebtoken")
+const secret = require ("../configs/auth.config")
+
 exports.signup = async (req, res) => {
     //Logic to create the user
 
@@ -28,4 +31,35 @@ exports.signup = async (req, res) => {
         }) //500 : internal server error
     }
     //3. Return the response back to the user
+}
+
+exports.signin =  async (req, res)=> {
+    //Check if the user id is present in the system
+    const user = await user_model.findOne({userId: req.body.userId})
+    
+    if (user == null){
+        return res.status(400).send({
+            message: "User id passed is not a valid user id"
+        })
+    } 
+
+    //Password is correct
+    const isPasswordValid = bcrypt.compareSync(req.body.password, user.password) //Enncrypts the first string and then compares it with the second string which is already encrypted.
+    if (!isPasswordValid){
+        return res.status(400).send({
+            message: "Wrong password passed"
+        })
+    } 
+    //Using JWT, access token is created with a given TTL and returned.
+    const token = jwt.sign({id: user.userId}, secret.secret, {
+        expiresIn: 120 //seconds
+    }) //here, the userId is being used to create the token
+
+    res.status(200).send({
+        name: user.name,
+        userId: user.userId,
+        email: user.email,
+        userType: user.userType,
+        accesstoken: token
+    })
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,4 +4,7 @@ const authController = require ("../controllers/auth.controller")
 const authMW = require ("../middlewares/auth.mw")
 module.exports = (app) =>{
     app.post ("/ecomm/api/v1/auth/signup", [authMW.verifySignUpBody], authController.signup)
+
+    //Route for POST localhost:8888/ecomm/api/v1/auth/signin
+    app.post("/ecomm/api/v1/auth/signin", authController.signin)
 }
